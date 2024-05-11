1b4be1bd8e71fa9ef8ea459dc5016705f202b074
zonggonau <cristoperzonggonau@gmail.com>
2024-05-11 21:19:24 +0900

    upgrade

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import withPWAInit from 'nextjs-pwa';
 /** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+    dest: 'public',
+})
 const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+export default withPWA({
+    nextConfig
+})
