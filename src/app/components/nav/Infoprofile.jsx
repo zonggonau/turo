993745dd724be7a23f002fd3cca922293a96a7e2
zonggonau <cristoperzonggonau@gmail.com>
2024-05-12 23:41:31 +0900

    upgrade

---
+++ src/app/components/nav/Infoprofile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Infoprofile() {
+    return (
+        <div className="border-2 p-3 flex space-x-3 items-center">
+            <div>
+                <img className="w-10 h-10 rounded-full" src="https://scontent.fdjj1-1.fna.fbcdn.net/v/t39.30808-1/297411088_10220191287804303_4443583481344375331_n.jpg?stp=dst-jpg_s200x200&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHQYmBoT2NNyx6SRBDjutSDn0YIPBxPjFufRgg8HE-MW1_FeZzLBhoT5sME6-NGAEw&_nc_ohc=sX8KVPlnVoQQ7kNvgETWpLD&_nc_ht=scontent.fdjj1-1.fna&oh=00_AYCGEzwgDsx1xJOBxKokyumJA6xfrl2nSBQm5t3VmaTGRQ&oe=66469A8F" />
+            </div>
+            <div>
+                <p className="text-xs">Selamat Datang</p>
+                <p className="text-xs">Kristovedus Zonggonau</p>
+            </div>
+        </div>
+    )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/nav";
+import Infoprofile from "./components/nav/Infoprofile";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -48,7 +50,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Infoprofile />
+        {children}
+        <Navbar />
+      </body>
     </html>
   );
 }
